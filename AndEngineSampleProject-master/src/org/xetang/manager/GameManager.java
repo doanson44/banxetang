03d266d7a21c7a9b42da28b5d9cf559489d54942
voanhkiem6 <voanhkiem6@e5badab9-f265-eebe-7b73-a64227dd29dd>
2014-11-13 10:56:25 +0000

    Hoàn tất MenuGame

--- AndEngineSampleProject-master/src/org/xetang/manager/GameManager.java
+++ AndEngineSampleProject-master/src/org/xetang/manager/GameManager.java
@@ -1,14 +1,31 @@
 package org.xetang.manager;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.andengine.opengl.texture.region.BaseTextureRegion;
+import org.andengine.audio.music.Music;
+import org.andengine.audio.music.MusicFactory;
+import org.andengine.audio.music.MusicManager;
 import org.andengine.engine.Engine;
 import org.andengine.engine.camera.Camera;
 import org.andengine.entity.scene.Scene;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.font.FontManager;
 import org.andengine.opengl.texture.TextureManager;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.color.Color;
 import org.xetang.main.MainActivity;
-import org.xetang.map.MapObjectFactory;
 import org.xetang.map.model.XMLLoader;
+import org.xetang.root.MainMenuScene;
 
 import android.content.res.AssetManager;
 
@@ -16,8 +33,9 @@ public class GameManager {
 
 	public static final String TANK_TAG = "XeTang"; // Dùng để debug
 
-	public static final int CAMERA_WIDTH = 1080;
-	public static final int CAMERA_HEIGHT = CAMERA_WIDTH / 10 * 6;
+
+	public static final int CAMERA_WIDTH = 800;
+	public static final int CAMERA_HEIGHT = 480;
 
 	public static final int MAP_WIDTH = 832;
 	public static final int MAP_HEIGHT = MAP_WIDTH / 4 * 3;
@@ -36,7 +54,6 @@ public class GameManager {
 		Up, Right, Down, Left
 	}
 
-	public static MainActivity Activity;
 	public static Camera Camera;
 	public static Scene Scene;
 	public static Engine Engine;
@@ -45,6 +62,21 @@ public class GameManager {
 	public static MainActivity Context;
 	public static VertexBufferObjectManager VertexBufferObject;
 	public static PhysicsWorld PhysicsWorld;
+	
+	/**
+	 * @editor: Nhân Bạch
+	 * @date: 13/11/2014
+	 * @brief: Thêm 1 số biến phục vụ cho việc làm MENU 
+	 */
+	public static FontManager FontManager;
+	public static MusicManager MusicManager;
+	static Hashtable<String, BaseTextureRegion> Textures;
+	static Hashtable<String, Font> Fonts;
+	static Hashtable<String, Music> Musics;
+	public static boolean IsBackgroundSound = true;
+	public static boolean IsEffectSound = true;
+	public static List<Scene> ListScene = new ArrayList<Scene>();
+	/**********************/
 
 	public static GameMapManager CurrentMapManager;
 
@@ -71,14 +103,27 @@ public class GameManager {
 	}
 
 	public static void loadResource() {
-		// GameMapManager.loadResource();
-		// TankManager.loadResouceAllTank();
-		// GameItemManager.loadResource();
-		// GameControllerManager.loadResource();
+		Textures = new Hashtable<String, BaseTextureRegion>();
+		Musics = new Hashtable<String, Music>();
+		Fonts = new Hashtable<String, Font>();
+		
+
+		
+		// Load Fonts
+		loadFonts();
+
+		// load Musics
+		loadMusics();
+		
+		//load Textures
+		loadTextures();
 
 		XMLLoader.loadAllParameters();
-		MapObjectFactory.initAllObjects();
+		// MapObjectFactory.initAllObjects();
+		
 	}
+
+	
 
 	/*
 	 * ĂN ĐI KU
@@ -88,10 +133,73 @@ public class GameManager {
 		 * Xử lý Scene cũ
 		 */
 
-		GameManager.Scene = newScene;
+		
+		
 	}
 
 	public static int getCurrentStage() {
 		return mStage;
 	}
+
+	
+	/**
+	 * @editor: Nhân Bạch
+	 * @date: 13/11/2014
+	 * @brief: Thêm 1 số hàm phục vụ cho việc làm MENU 
+	 */
+	private static void loadTextures() {
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/menu/");
+		BitmapTextureAtlas atlas = new BitmapTextureAtlas(GameManager.TextureManager, 100, 50, TextureOptions.BILINEAR);
+		TiledTextureRegion t = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(atlas, GameManager.AssetManager, "sound.png",0 ,0, 2, 1);
+		Textures.put("sound", t);
+		atlas.load();
+	}
+
+	private static void loadMusics() {
+		try {
+			MusicFactory.setAssetBasePath("music/");
+			Music m;
+			m = MusicFactory.createMusicFromAsset(GameManager.MusicManager,
+					GameManager.Context, "menu.mp3");
+			Musics.put("menu", m);
+			m = MusicFactory.createMusicFromAsset(GameManager.MusicManager,
+					GameManager.Context, "blop.mp3");
+			Musics.put("blop", m);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void loadFonts() {
+		FontFactory.setAssetBasePath("font/");
+		Font f = FontFactory.createFromAsset(GameManager.FontManager,
+				GameManager.TextureManager, 256, 256,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA,
+				GameManager.AssetManager, "font1.ttf", 64f, true,
+				Color.WHITE_ABGR_PACKED_INT);
+		Fonts.put("font1", f);
+		f = FontFactory.createFromAsset(GameManager.FontManager,
+				GameManager.TextureManager,
+				(int) GameManager.Camera.getWidth(),
+				(int) GameManager.Camera.getHeight(),
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA,
+				GameManager.AssetManager, "font2.ttf", 24f, true,
+				Color.WHITE_ABGR_PACKED_INT);
+		Fonts.put("font2", f);
+	}
+	
+	public static BaseTextureRegion getTexture(String key) {
+		return Textures.get(key);
+	}
+
+	public static Music getMusic(String key) {
+		return Musics.get(key);
+	}
+
+	public static Font getFont(String key) {
+		return Fonts.get(key);
+	}
+	/**********************/
+	
+	
 }
